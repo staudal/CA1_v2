ebce2c1a3675f305b7e8008e7aefc34854c7a537
Jakob Staudal <jakob@staudal.dev>
2023-03-16 08:31:53 +0100

    Added CORS to HobbyResource (DELETE)

--- src/main/java/rest/HobbyResource.java
+++ src/main/java/rest/HobbyResource.java
@@ -76,6 +76,14 @@ public class HobbyResource {
     @Consumes({MediaType.APPLICATION_JSON})
     public Response deleteHobby(@PathParam("id") Long id) {
         HobbyDTO hobbyDTO = FACADE.deleteHobby(id);
-        return Response.ok().entity(GSON.toJson(hobbyDTO)).build();
+        return Response
+        .ok()
+        .header("Access-Control-Allow-Origin", "*")
+        .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+        .header("Access-Control-Allow-Credentials", "true")
+        .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+        .header("Access-Control-Max-Age", "1209600")
+        .entity(GSON.toJson(hobbyDTO))
+        .build();
     }
 }
